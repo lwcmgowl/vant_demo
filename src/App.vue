aa8dbc6baf43af08636f8487400966b89a9371f4
liwei <>
2020-10-27 22:01:23 +0800

    tj

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
+    <van-tabbar route v-model="active">
+      <van-tabbar-item replace to="/" icon="home-o"> 首页 </van-tabbar-item>
+       <van-tabbar-item replace to="/Classify" icon="apps-o"> 分类 </van-tabbar-item>
+        <van-tabbar-item replace to="/Find" icon="send-gift-o"> 发现 </van-tabbar-item>
+         <van-tabbar-item replace to="/Cart" icon="shopping-cart-o"> 购物车 </van-tabbar-item>
+      <van-tabbar-item replace to="/User" icon="user-circle-o">
+        我的
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
-
+<script>
+import { Tabbar, TabbarItem } from 'vant'
+export default {
+  data () {
+    return {
+      active: 0
+    }
+  },
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  }
+}
+</script>
 <style lang="scss">
 </style>
---
+++ src/views/Classify.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <van-sidebar v-model="activeKey">
+      <van-sidebar-item title="推荐分类" ></van-sidebar-item>
+      <van-sidebar-item title="超市" ></van-sidebar-item>
+      <van-sidebar-item title="男装" ></van-sidebar-item>
+    </van-sidebar>
+  </div>
+</template>
+<script>
+import { Sidebar, SidebarItem } from 'vant'
+
+export default {
+  data () {
+    return {
+      activeKey: 0
+    }
+  },
+  components: {
+    [Sidebar.name]: Sidebar,
+    [SidebarItem.name]: SidebarItem
+  }
+}
+</script>
